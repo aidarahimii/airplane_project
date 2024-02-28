f6ac5a6cf012a163158b79cfb1420643e0d4f498
AidaRahimi <aida83.rahimi@gmail.com>
2024-02-28 19:13:27 +0330

    database changed

--- src/airline/model/da/db.sql
+++ src/airline/model/da/db.sql
@@ -3,11 +3,14 @@ create table passenger_tbl
     id           number,
     name         nvarchar2(20),
     family       nvarchar2(20),
-    address      nvarchr2(60),
+    address      nvarchar2(60),
     national_cod nvarchar2(10),
-    phone_number nvarchar2(12)
+    phone_number nvarchar2(12),
+    capacity    number(3)
+
 );
 create sequence passenger_seq start with 1 increment by 1;
+
 
 create table flight_tbl
 (
@@ -17,17 +20,17 @@ create table flight_tbl
     destination nvarchar2(30),
     end_time    timestamp,
     airline_id  number,
-    capacity    number(3)
+    duration    number(4)
 );
 create sequence flight_seq start with 1 increment by 1;
 
 create table airline_tbl
 (
     id      number,
-    name    nvarchar2(20),
-    phone   nvarchar2(20),
-    address nvarchar2(20)
-)
+    airline    nvarchar2(20),
+    name   nvarchar2(20),
+    model  nvarchar2(20)
+);
 create sequence airline_seq start with 1 increment by 1;
 
 
@@ -38,6 +41,6 @@ create table ticket_tbl
     passenger_id number,
     flight_id    number,
     price        number,
-    position     nvarchar(5)
+    position     nvarchar2(5)
 );
 create sequence ticket_seq start with 1 increment by 1;
